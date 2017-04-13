eeccf02b7b13e0abfa0a0b054608f8a16a86b8e2
Spencer Smith <SpencerMKSmith>
2017-04-12 18:59:50 -0700

    Added events.js.  Use this to subscribe to events from other components.

---
+++ assets/js/events.js
@@ -0,0 +1,16 @@
+
+/*
+ * All interactions should be routed through this file so that the components
+ * are connected but decoupled.  Add events if necessary
+ */
+
+function didClickChoroplethMap(regionName) {
+    console.log(regionName);
+
+
+}
+
+function didChangeCitycityName() {
+
+}
+
